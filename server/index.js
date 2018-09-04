03e05fac8a52334d0280475b277d3416b72308c3
Salad <xa.lach@yahoo.com>
2018-09-04 21:14:19 +0700

    Revert last change

--- server/index.js
+++ server/index.js
@@ -4,9 +4,6 @@ const fs = require("fs");
 const app = express();
 const dotenv = require("dotenv");
 
-const rootDir = path.join(__dirname, "..", "build");
-const indexPath = path.join(rootDir, "index.html");
-
 dotenv.config();
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 app.use(require("./middleware").middleware);
@@ -14,6 +11,8 @@ app.use(require("./middleware").middleware);
 const { HTTP_STATUS_CODE } = require("./common/constants").constants;
 
 if (process.env.NODE_ENV === "production") {
+  const rootDir = path.join(__dirname, "..", "build");
+  const indexPath = path.join(rootDir, "index.html");
   const indexHtml = fs
     .readFileSync(indexPath, "utf8")
     .replace(/%ORIGIN%/g, process.env.ORIGIN)
@@ -23,6 +22,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(rootDir));
 }
 
-app.use("*", (req, res) => res.status(HTTP_STATUS_CODE.OK).sendFile(indexPath));
+app.use("*", (req, res) =>
+  res.status(HTTP_STATUS_CODE.OK).send("You went wrong.")
+);
 
 app.listen(process.env.PORT || 4000);
