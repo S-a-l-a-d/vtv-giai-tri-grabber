316fd5719bf1d1fdbb5281adec7049193ca15002
Salad <xa.lach@yahoo.com>
2018-09-04 20:44:05 +0700

    Try fixing API routes

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ const fs = require("fs");
 const app = express();
 const dotenv = require("dotenv");
 
+const rootDir = path.join(__dirname, "..", "build");
+const indexPath = path.join(rootDir, "index.html");
+
 dotenv.config();
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 app.use(require("./middleware").middleware);
@@ -11,8 +14,6 @@ app.use(require("./middleware").middleware);
 const { HTTP_STATUS_CODE } = require("./common/constants").constants;
 
 if (process.env.NODE_ENV === "production") {
-  const rootDir = path.join(__dirname, "..", "build");
-  const indexPath = path.join(rootDir, "index.html");
   const indexHtml = fs
     .readFileSync(indexPath, "utf8")
     .replace(/%ORIGIN%/g, process.env.ORIGIN)
@@ -22,8 +23,6 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(rootDir));
 }
 
-app.use("*", (req, res) =>
-  res.status(HTTP_STATUS_CODE.OK).send("You went wrong.")
-);
+app.use("*", (req, res) => res.status(HTTP_STATUS_CODE.OK).sendFile(indexPath));
 
 app.listen(process.env.PORT || 4000);
